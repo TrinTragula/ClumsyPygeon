461011ec7bec38b96ffeabe5780e06d84a386000
TrinTragula <daniele.scarinci@protonmail.com>
2016-12-01 18:31:58 -0500

    Ora manca solo l'enpassant

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ from boardvalue import boardvalue
 from boardvalue import boardvaluenero
 
 # Rappresentazione della scacchiera
-boardbbbbb = "0000000k" \
-        "R0R00000" \
-        "00000000" \
-        "00000000" \
-        "00000000" \
-        "00000000" \
-        "PPPPPPPP" \
-        "RNBQKBNR"
-
-
-
 board = "rnbqkbnr" \
         "pppppppp" \
         "00000000" \
@@ -57,32 +46,66 @@ movelist = []
 def ischeck(start, end):
     global board
     global tratto
-    trattobackup = tratto
-    boardbackup = board
+    global arroccoNero
+    global arroccoBianco
+    global arroccoNeroLungo
+    global arroccoNeroCorto
+    global arroccoBiancoLungo
+    global arroccoBiancoCorto
 
+    arroccoNerob = arroccoNero
+    arroccoBiancob = arroccoBianco
+    arroccoNeroLungob = arroccoNeroLungo
+    arroccoNeroCortob = arroccoNeroCorto
+    arroccoBiancoLungob = arroccoBiancoLungo
+    arroccoBiancoCortob = arroccoBiancoCorto
+    trattob = tratto
+    boardb = board
+    ren = board.index("k")
+    reb = board.index("K")
+    if end == ren or end == reb:
+        return 1
     move(start,end)
-
     ren = board.index("k")
     reb = board.index("K")
 
+
     if tratto == 1 :
-        for i in range(len(board)):
+        for i in range(64):
             if not islegal( i, reb ):
-                move(end,start)
-                board = boardbackup
-                tratto = trattobackup
+                # ripristino le condizioni varie
+                board = boardb
+                tratto = trattob
+                arroccoNero = arroccoNerob
+                arroccoBianco = arroccoBiancob
+                arroccoNeroLungo = arroccoNeroLungob
+                arroccoNeroCorto = arroccoNeroCortob
+                arroccoBiancoLungo = arroccoBiancoLungob
+                arroccoBiancoCorto = arroccoBiancoCortob
                 return 1
     if tratto == 0 :
-        for i in range(len(board)):
+        for i in range(64):
             if not islegal( i, ren ):
-                move(end,start)
-                board = boardbackup
-                tratto = trattobackup
+                # ripristino le condizioni varie
+                board = boardb
+                tratto = trattob
+                arroccoNero = arroccoNerob
+                arroccoBianco = arroccoBiancob
+                arroccoNeroLungo = arroccoNeroLungob
+                arroccoNeroCorto = arroccoNeroCortob
+                arroccoBiancoLungo = arroccoBiancoLungob
+                arroccoBiancoCorto = arroccoBiancoCortob
                 return 1
 
-    move(end,start)
-    board = boardbackup
-    tratto = trattobackup
+    # ripristino le condizioni varie
+    board = boardb
+    tratto = trattob
+    arroccoNero = arroccoNerob
+    arroccoBianco = arroccoBiancob
+    arroccoNeroLungo = arroccoNeroLungob
+    arroccoNeroCorto = arroccoNeroCortob
+    arroccoBiancoLungo = arroccoBiancoLungob
+    arroccoBiancoCorto = arroccoBiancoCortob
     return 0
 
 # ---------------------------------------------------
@@ -112,6 +135,7 @@ def islegal( start, end ):
     # Se si vuole muovere una casella vuota, illegale
     if board[start] == "0":
         return 1
+
 
  ###################################
  #            PEZZI NERI
@@ -188,11 +212,11 @@ def islegal( start, end ):
             if      (end != (start + 8 + 8 + 1) or (start/8 != (end/8 -2))) \
                 and (end != (start + 8 + 8 - 1) or (start/8 != (end/8 -2))) \
                 and (end != (start + 1 + 1 + 8) or (start/8 != (end/8 -1))) \
-                and (end != (start + 1 + 1 - 8) or (start/8 != (end/8 -1))) \
-                and (end != (start - 8 - 8 + 1) or (start/8 != (end/8 -2))) \
-                and (end != (start - 8 - 8 - 1) or (start/8 != (end/8 -2))) \
+                and (end != (start + 1 + 1 - 8) or (start/8 != (end/8 +1))) \
+                and (end != (start - 8 - 8 + 1) or (start/8 != (end/8 +2))) \
+                and (end != (start - 8 - 8 - 1) or (start/8 != (end/8 +2))) \
                 and (end != (start - 1 - 1 + 8) or (start/8 != (end/8 -1))) \
-                and (end != (start - 1 - 1 - 8) or (start/8 != (end/8 -1))):
+                and (end != (start - 1 - 1 - 8) or (start/8 != (end/8 +1))):
                 return 1
             return 0
 
@@ -200,17 +224,47 @@ def islegal( start, end ):
         # RE NERO
         #############################
         if board[start] == "k":
-
-            # Il re si muove di uno in qualsivoglia direzione
-            if      ((end != (start + 1) and end != (start - 1)) or (start/8 != end/8)) \
+         # Nel caso sia possibile l'arroco
+         if arroccoNero == 0:
+            #vari arrocchi possibili
+            # Il re si muove di uno in qualsivoglia direzione o arrocca
+            if      ( (end != (start + 1)) or (start/8    != end/8)) \
+                and ( (end != (start - 1)) or (start/8    != end/8)) \
                 and ( (end != (start + 8)) or (start/8 +1 != end/8)) \
-                and ( (end != (start - 8)) or (start/8 -1 != end/8))\
-                and ( (end != (start + 9)) or (start/8 +1 != end/8))\
+                and ( (end != (start - 8)) or (start/8 -1 != end/8)) \
+                and ( (end != (start + 9)) or (start/8 +1 != end/8)) \
+                and ( (end != (start + 7)) or (start/8 +1 != end/8)) \
+                and ( (end != (start - 9)) or (start/8 -1 != end/8)) \
+                and ( (end != (start - 7)) or (start/8 -1 != end/8)):
+
+                if arroccoNeroCorto == 0   \
+                    and board[5]=="0" and board[6]=="0" \
+                    and start == 4 and end == 6\
+                    and not ischeck(4,5) and not ischeck(4,6):
+                    return 0
+                if arroccoNeroLungo == 0   \
+                    and board[1]=="0" and board[2]=="0" and board[3]=="0"\
+                    and start == 4 and end == 2\
+                    and not ischeck(4,1) and not ischeck(4,2) and not ischeck(4,3):
+                    return 0
+                #se non è una mossa possibile o un arrocco, ritorni uno
+                return 1
+            return 0
+
+         if arroccoNero != 0:
+            #vari arrocchi possibili
+            # Il re si muove di uno in qualsivoglia direzione o arrocca
+            if      ( (end != (start + 1)) or (start/8    != end/8)) \
+                and ( (end != (start - 1)) or (start/8    != end/8)) \
+                and ( (end != (start + 8)) or (start/8 +1 != end/8)) \
+                and ( (end != (start - 8)) or (start/8 -1 != end/8)) \
+                and ( (end != (start + 9)) or (start/8 +1 != end/8)) \
                 and ( (end != (start + 7)) or (start/8 +1 != end/8)) \
                 and ( (end != (start - 9)) or (start/8 -1 != end/8)) \
                 and ( (end != (start - 7)) or (start/8 -1 != end/8)):
                 return 1
             return 0
+
 
         ##############################
         # ALFIERE NERO
@@ -454,11 +508,43 @@ def islegal( start, end ):
         # RE BIANCO
         #############################
         if board[start] == "K":
-            # Il re si muove di uno in qualsivoglia direzione
-            if      ((end != (start + 1) and end != (start - 1)) or (start/8 != end/8)) \
+         # Nel caso sia possibile l'arroco
+         if arroccoBianco == 0:
+            #vari arrocchi possibili
+            # Il re si muove di uno in qualsivoglia direzione o arrocca
+            if      ( (end != (start + 1)) or (start/8    != end/8)) \
+                and ( (end != (start - 1)) or (start/8    != end/8)) \
                 and ( (end != (start + 8)) or (start/8 +1 != end/8)) \
-                and ( (end != (start - 8)) or (start/8 -1 != end/8))\
-                and ( (end != (start + 9)) or (start/8 +1 != end/8))\
+                and ( (end != (start - 8)) or (start/8 -1 != end/8)) \
+                and ( (end != (start + 9)) or (start/8 +1 != end/8)) \
+                and ( (end != (start + 7)) or (start/8 +1 != end/8)) \
+                and ( (end != (start - 9)) or (start/8 -1 != end/8)) \
+                and ( (end != (start - 7)) or (start/8 -1 != end/8)):
+
+                if arroccoBiancoCorto == 0   \
+                    and board[62]=="0" and board[61]=="0" \
+                    and start == 60 and end == 62\
+                    and not ischeck(60,61) and not ischeck(60,62):
+                    return 0
+                if arroccoBiancoLungo == 0   \
+                    and board[59]=="0" and board[58]=="0" and board[57]=="0"\
+                    and start == 60 and end == 58\
+                    and not ischeck(60,59) and not ischeck(60,58) and not ischeck(60,57):
+                    return 0
+                #se non è una mossa possibile o un arrocco, ritorni uno
+                return 1
+
+            return 0
+
+
+         if arroccoBianco != 0:
+            #vari arrocchi possibili
+            # Il re si muove di uno in qualsivoglia direzione o arrocca
+            if      ( (end != (start + 1)) or (start/8    != end/8)) \
+                and ( (end != (start - 1)) or (start/8    != end/8)) \
+                and ( (end != (start + 8)) or (start/8 +1 != end/8)) \
+                and ( (end != (start - 8)) or (start/8 -1 != end/8)) \
+                and ( (end != (start + 9)) or (start/8 +1 != end/8)) \
                 and ( (end != (start + 7)) or (start/8 +1 != end/8)) \
                 and ( (end != (start - 9)) or (start/8 -1 != end/8)) \
                 and ( (end != (start - 7)) or (start/8 -1 != end/8)):
@@ -634,8 +720,6 @@ def movegen():
     movelist = []
     for i in range(64):
         if tratto == 1:
-            #arrocco
-            if arroccoNero == 0:
             if board[i] in black:
                 for j in range(64):
                     if not islegal(i,j):
@@ -718,10 +802,24 @@ def search(depth, alpha, beta):
     global bestmove
     global board
     global tratto
+    global arroccoNero
+    global arroccoBianco
+    global arroccoNeroLungo
+    global arroccoNeroCorto
+    global arroccoBiancoLungo
+    global arroccoBiancoCorto
+
+    arroccoNerob = arroccoNero
+    arroccoBiancob = arroccoBianco
+    arroccoNeroLungob = arroccoNeroLungo
+    arroccoNeroCortob = arroccoNeroCorto
+    arroccoBiancoLungob = arroccoBiancoLungo
+    arroccoBiancoCortob = arroccoBiancoCorto
+
     mossa = (0,0)
     best = -999999999
-    boardbackup = board
-    trattobackup= tratto
+    boardb = board
+    trattob= tratto
     listamosse = movegen()
     if not listamosse:
         return -999999999999
@@ -735,8 +833,17 @@ def search(depth, alpha, beta):
             score = evaluate()
         else:
             score = - search(depth - 1, -beta, -alpha)
+
+        # ripristino le condizioni varie
         board = boardb
         tratto = trattob
+        arroccoNero = arroccoNerob
+        arroccoBianco = arroccoBiancob
+        arroccoNeroLungo = arroccoNeroLungob
+        arroccoNeroCorto = arroccoNeroCortob
+        arroccoBiancoLungo = arroccoBiancoLungob
+        arroccoBiancoCorto = arroccoBiancoCortob
+
         if (score == 999999999999):
             best = score
             mossa = i
@@ -840,36 +947,71 @@ def move(start, end):
     #move a piece in the board
     global board
     global tratto
+    global arroccoNero
+    global arroccoBianco
+    global arroccoNeroLungo
+    global arroccoNeroCorto
+    global arroccoBiancoLungo
+    global arroccoBiancoCorto
+
+    #condizioni per l'arrocco
+    if board[start] == "k":
+        arroccoNero += 1
+    if board[start] == "K":
+        arroccoBianco += 1
+    if start == 0:
+        arroccoNeroLungo += 1
+    if start == 7:
+        arroccoNeroCorto += 1
+    if start == 63:
+        arroccoBiancoLungo += 1
+    if start == 56:
+        arroccoBiancoCorto += 1
 
     #arrocco lungo nero
     if start == 4 and end == 2 and board[start] == "k":
-        move(4,3)
-        move(3,2)
-        move(0,1)
-        move(1,3)
+        scacchiera = list(board)
+        scacchiera[0] = "0"
+        scacchiera[1] = "0"
+        scacchiera[2] = "k"
+        scacchiera[3] = "r"
+        scacchiera[4] = "0"
+        board = "".join(scacchiera)
         return 0
     #arroco corto nero
     if start == 4 and end == 6 and board[start] == "k":
-        move(4,5)
-        move(5,6)
-        move(7,4)
-        move(4,5)
+        scacchiera = list(board)
+        scacchiera[7] = "0"
+        scacchiera[6] = "k"
+        scacchiera[5] = "r"
+        scacchiera[4] = "0"
+        board = "".join(scacchiera)
         return 0
     #arrocco lungo bianco
     if start == 60 and end == 58 and board[start] == "K":
-        move(60,59)
-        move(59,58)
-        move(56,57)
-        move(57,59)
+        scacchiera = list(board)
+        scacchiera[56] = "0"
+        scacchiera[57] = "0"
+        scacchiera[58] = "K"
+        scacchiera[59] = "R"
+        scacchiera[60] = "0"
+        board = "".join(scacchiera)
+        tratto += 1
+        if tratto > 1:
+            tratto = 0
         return 0
     #arroco corto bianco
     if start == 60 and end == 62 and board[start] == "K":
-        move(60,61)
-        move(61,62)
-        move(63,60)
-        move(60,61)
+        scacchiera = list(board)
+        scacchiera[63] = "0"
+        scacchiera[62] = "K"
+        scacchiera[61] = "R"
+        scacchiera[60] = "0"
+        board = "".join(scacchiera)
+        tratto += 1
+        if tratto > 1:
+            tratto = 0
         return 0
-
 
     scacchiera = list(board)
     scacchiera[end] = board[start]
@@ -881,28 +1023,12 @@ def move(start, end):
         tratto = 0
     return 0
 
-# routine to make life easier when implemeting uci
+# routine to make life easier when implemeting xboard
 def routine(start,end):
     #routine date mosse iniziali e finali
     #così per rendere piu semplice la futura implementazione uci
-
     if not islegal(start,end):
         if not ischeck(start,end):
-
-            #condizioni per l'arrocco
-            if board[start] == "k":
-                arroccoNero += 1
-            if board[start] == "K":
-                arroccoBianco += 1
-            if start == 0:
-                arroccoNeroLungo += 1
-            if start == 7:
-                arroccoNeroCorto += 1
-            if start == 63:
-                arroccoBiancoLungo += 1
-            if start == 56
-                arroccoBiancoCorto += 1
-
             move(start,end)
         else:
             print "Non puoi, andresti sotto scacco!"
@@ -947,10 +1073,10 @@ def gioco_ascii(lato, deep):
                     break
 
                 start, end = notationToCase(notazione)
-            except :
-                print "Notazione non compresa"
-                print "Usa notazione algebrica del tipo 'e2e4'"
-                continue
+            except Exception,e: print str(e)
+                #print "Notazione non compresa"
+                #print "Usa notazione algebrica del tipo 'e2e4'"
+                #continue
 
             routine(start,end)
 
@@ -959,7 +1085,6 @@ def gioco_ascii(lato, deep):
             print "Arrivederla!"
 
     # Se voglio giocare come nero in ascii
-    tratto = 0
     if lato == "nero":
         try:
          while(1):
@@ -1010,5 +1135,8 @@ def inizio():
     scelta = raw_input()
     if scelta.partition(' ')[0] == "ascii":
         gioco_ascii(scelta.partition(' ')[2],2)
+
 tratto = 0
+
+
 inizio()
