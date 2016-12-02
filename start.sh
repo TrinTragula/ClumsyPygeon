6377b568f8c846131f2585591265b01911e0666b
TrinTragula <daniele.scarinci@protonmail.com>
2016-12-02 03:39:01 -0500

    Lenta ma pronta

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf xboard.debug
-xboard -debugMode true -cp -fcp "python main.py" -scp "python main.py"
+rm -rf Clumsy_log_xboard.txt
+xboard -debugMode true -cp -fcp "python src/main.py" -scp "python src/main.py"
